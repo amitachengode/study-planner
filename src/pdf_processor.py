029280877fa4192655168b028a35d48ed0d585b7
Amit A Chengode <amitachengode@gmail.com>
2026-06-28 13:09:55 +0530

    [append]: added pdf processor

---
+++ src/pdf_processor.py
@@ -0,0 +1,33 @@
+import pdfplumber
+import json
+
+
+def read_and_pdf(pdf_path):
+    text = ""
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            text += page.extract_text()
+    
+    return text
+        
+
+def generate_chunks(text: str, sample_name: str, chunk_size=100) -> list[tuple[str, str]]:
+    overlap = chunk_size//5
+    words = text.split()
+    chunks = []
+    
+    i = 0
+    chunk_count = 1
+    while i < len(words):
+        chunk_words = words[i : i + chunk_size]
+        chunk_text = " ".join(chunk_words)
+        chunk_label = f"{sample_name}-chunk-{chunk_count}"
+        chunks.append((chunk_text, chunk_label))
+        chunk_count += 1
+        i += (chunk_size - overlap)
+        
+    return chunks
+
+def generate_chunks_from_pdf(pdf_path: str, chunk_size=150) -> list[tuple[str, str]]:
+    text = read_and_pdf(pdf_path)
+    return generate_chunks(text, pdf_path, chunk_size)
